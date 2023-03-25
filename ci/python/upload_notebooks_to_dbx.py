43b8aab3f24f3af1c6f52b8ec99e283a33e72822
Shaun Ryan <shaunryan@WorlordV.local>
2023-03-25 19:56:15 +0000

    notebook deploy

--- ci/python/upload_notebooks_to_dbx.py
+++ ci/python/upload_notebooks_to_dbx.py
@@ -2,9 +2,17 @@
 import os
 from autobricks import Workspace
 
-Workspace.workspace_import_dir(
-    from_notebook_root="",
-    source_dir="",
-    target_dir="autobricks",
-    deploy_mode=Workspace.DeployMode.PARENT
-)
+ROOT_DIR = os.getenv("ROOT_DIR")
+WORKSPACE_ROOT = os.getenv("WORKSPACE_ROOT")
+
+source_dir = f"{ROOT_DIR}/Databricks/Notebooks"
+target_dir = f"/{WORKSPACE_ROOT}"
+
+print(f"importing workspace files from {source_dir} to {target_dir}")
+
+# Workspace.workspace_import_dir(
+#     from_notebook_root=source_dir,
+#     source_dir=target_dir,
+#     target_dir="autobricks",
+#     deploy_mode=Workspace.DeployMode.PARENT
+# )
