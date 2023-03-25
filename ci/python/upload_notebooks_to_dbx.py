10dc2f4612312c1d159227eb42fd632dc36ba089
Shaun Ryan <shaunryan@WorlordV.local>
2023-03-25 19:49:40 +0000

    notebook deploy

--- ci/python/upload_notebooks_to_dbx.py
+++ ci/python/upload_notebooks_to_dbx.py
@@ -1,13 +1,10 @@
 # from autobricks import Workspace
 import os
+from autobricks import Workspace
 
-print(os.getenv("TENANT_ID"))
-print(os.getenv("SP_CLIENT_ID"))
-print(os.getenv("SP_CLIENT_SECRET"))
-
-# Workspace.workspace_import_dir(
-#     from_notebook_root="",
-#     source_dir="",
-#     target_dir="autobricks",
-#     deploy_mode=Workspace.DeployMode.PARENT
-# )
+Workspace.workspace_import_dir(
+    from_notebook_root="",
+    source_dir="",
+    target_dir="autobricks",
+    deploy_mode=Workspace.DeployMode.PARENT
+)
