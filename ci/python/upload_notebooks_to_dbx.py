0b7e54217a0212451907a4d5c4456a31850e7d1c
Shaun Ryan <shaunryan@WorlordV.local>
2023-03-26 08:51:14 +0100

    notebook deploy

--- ci/python/upload_notebooks_to_dbx.py
+++ ci/python/upload_notebooks_to_dbx.py
@@ -6,7 +6,7 @@ WORKSPACE_ROOT = os.getenv("WORKSPACE_ROOT")
 WORKSPACE_SUBDIRS:str = os.getenv("WORKSPACE_SUBDIRS")
 
 from_notebook_root = f"{ROOT_DIR}/Databricks/Notebooks/"
-target_dir = f"/{WORKSPACE_ROOT}/"
+target_dir = f"/{WORKSPACE_ROOT}"
 sub_folders = [f"/{d.strip()}" for d in WORKSPACE_SUBDIRS.split(",")]
 
 for source_dir in sub_folders:
