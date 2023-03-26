9b5cc8ba166005f9bb0f2d10b4d1b32b6656979f
Shaun Ryan <shaunryan@WorlordV.local>
2023-03-26 07:50:54 +0100

    notebook deploy

--- ci/python/upload_notebooks_to_dbx.py
+++ ci/python/upload_notebooks_to_dbx.py
@@ -3,15 +3,17 @@ from autobricks import Workspace
 
 ROOT_DIR = os.getenv("ROOT_DIR")
 WORKSPACE_ROOT = os.getenv("WORKSPACE_ROOT")
+WORKSPACE_SUBDIRS:str = os.getenv("WORKSPACE_SUBDIRS")
 
-source_dir = f"{ROOT_DIR}"
+from_notebook_root = f"{ROOT_DIR}/Databricks/Notebooks"
 target_dir = f"/{WORKSPACE_ROOT}/"
+sub_folders = [d.strip() for d in WORKSPACE_SUBDIRS.split(",")]
 
-print(f"importing workspace files from {source_dir} to {target_dir}")
+for f in sub_folders:
 
-Workspace.workspace_import_dir(
-    from_notebook_root=source_dir,
-    source_dir="/Databricks/Notebooks/",
-    target_dir=target_dir,
-    deploy_mode=Workspace.DeployMode.MOVE
-)
+    Workspace.workspace_import_dir(
+        from_notebook_root=from_notebook_root,
+        source_dir=f"/{f}/",
+        target_dir=target_dir,
+        deploy_mode=Workspace.DeployMode.PARENT
+    )
