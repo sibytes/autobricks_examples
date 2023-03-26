c990b1d70f8c9476a026d476d85835fbe13af3c9
Shaun Ryan <shaunryan@WorlordV.local>
2023-03-26 08:35:34 +0100

    notebook deploy

--- main.py
+++ main.py
@@ -7,15 +7,11 @@ WORKSPACE_SUBDIRS:str = os.getenv("WORKSPACE_SUBDIRS")
 
 from_notebook_root = f"{ROOT_DIR}Databricks/Notebooks/"
 target_dir = f"/{WORKSPACE_ROOT}/"
-sub_folders = [f"/{d.strip()}" for d in WORKSPACE_SUBDIRS.split(",")]
+sub_folders = [d.strip() for d in WORKSPACE_SUBDIRS.split(",")]
 
-for source_dir in sub_folders:
+for f in sub_folders:
 
-    # this will iterate all dirs in the from_notebook_root
-    # if the root of the dir matches the source_dir then it
-    # will deploy the folder otherwise it will skip it.
-    # Deploy mode is PARENT it will copy the source dir 
-    # into the target dir in the worksapce
+    source_dir = f"/{f}"
     Workspace.workspace_import_dir(
         from_notebook_root=from_notebook_root,
         source_dir=source_dir,
